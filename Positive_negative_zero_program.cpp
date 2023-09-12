66b48031e8d45363c7fc0960a3f4f357595898dc
VAMSHI PALLAPU <44836993+Vamshipallapu@users.noreply.github.com>
2023-09-13 00:00:23 +0530

    Add files via upload

---
+++ Positive_negative_zero_program.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+    if (n>0)
+        cout<<"positive";
+    else if (n<0)
+        cout<<"Negative";
+    else
+        cout<<"Zero"; 
+}
