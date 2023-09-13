25876665c8388a13a8e16811b661ab5ea42a2e82
VAMSHI PALLAPU <44836993+Vamshipallapu@users.noreply.github.com>
2023-09-13 21:42:32 +0530

    Add files via upload

---
+++ 2_using_functions_2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void greetmsg()
+{
+    cout<<"Hi";
+    cout<<"welcome to gfg";
+}
+void exitmsg()
+{
+    cout<<"thank you";
+    cout<<"visit again";
+}
+int main()
+{
+    greetmsg();
+    cout<<"Hope u enjoyed";
+    exitmsg();
+    return 0;
+}
